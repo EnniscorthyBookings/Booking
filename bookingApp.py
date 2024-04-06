41a1fef4d248ac9c2f04dcea367795336c887547
BookingsStreamlit <166061925+BookingsStreamlit@users.noreply.github.com>
2024-04-06 13:57:41 +0100

    Update bookingApp.py

--- bookingApp.py
+++ bookingApp.py
@@ -114,8 +114,9 @@ def book_room():
     st.header("Book a Room or a Desk")
     date = st.date_input("Select the Date:", min_value=current_time_ireland.date(), value=None, format="DD/MM/YYYY")
     current_date = current_time_ireland.date()
-    st.write(date)
+    
     if date:
+        st.write(date)
         office_start_time = datetime.time(8, 0)
         office_end_time = datetime.time(20, 0)
         start_times = [office_start_time]
