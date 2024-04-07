2f0557d826e796ab0f3d71b740491e45f6d52485
BookingsStreamlit <166061925+BookingsStreamlit@users.noreply.github.com>
2024-04-07 14:43:54 +0100

    Update bookingApp.py

--- bookingApp.py
+++ bookingApp.py
@@ -686,7 +686,7 @@ if width > 800:
     if menu_choice == "Book a Room or Desk":
         book_room()
     elif menu_choice == "Cancel Booking":
-        cancel_room()
+        cancel_room(booking_data, update_booking_csv)
     elif menu_choice == "View Bookings":
         view_reservations()
 else:
@@ -705,7 +705,7 @@ else:
     if menu_choice == "Book a Room or Desk":
         book_room()
     elif menu_choice == "Cancel Booking":
-        cancel_room()
+        cancel_room(booking_data, update_booking_csv)
     elif menu_choice == "View Bookings":
         view_reservations()
 
