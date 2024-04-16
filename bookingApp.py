819e584790ed30c02c4593b5ab7707730a9116a6
EnniscorthyBookings <167249180+EnniscorthyBookings@users.noreply.github.com>
2024-04-16 22:46:58 +0100

    Update bookingApp.py

--- bookingApp.py
+++ bookingApp.py
@@ -156,10 +156,12 @@ def book_room():
             repo = g.get_repo("ohmydaysOMD/test")  # Assuming 'g' is your authenticated GitHub instance
             file2 = repo.get_contents(file_path2, ref=branch_name)
             st.write(file2)
-            image_bytes = file2.decoded_content
+            image = Image.open(file2)
+            st.image(image, channels="RGB", use_column_width=True)
+            #image_bytes = file2.decoded_content
             
             # Display the image
-            st.image(image_bytes, caption="Floor Layout")
+            #st.image(image_bytes, caption="Floor Layout")
             
         except Exception as e:
             st.error(f"Error: {e}")
